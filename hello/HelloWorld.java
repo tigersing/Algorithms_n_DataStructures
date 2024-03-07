8c8ea5e88b9da12b9cfbca55a53274823fbc10da
tigersing <tigersing.java@gmail.com>
2024-03-07 13:16:00 -0500

    new Commit

---
+++ hello/HelloWorld.java
@@ -0,0 +1,11 @@
+/** Represents a greeting printer */
+public class HelloWorld {
+  /**
+   * Prints "Hello, World"
+   *
+   * @param args user inputs - not used
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
